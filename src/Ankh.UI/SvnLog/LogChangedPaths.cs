chucker/AnkhSVN

commit 55339626c1dcc47af4b1b1ff475ff80f7715cffc

Clean up log control event routing. (Use an eventargs object in all events)
* src/Ankh.Services/UI/ICurrentItemSource.cs
  Update events prototype
* src/Ankh.UI/MergeWizard/MergeRevisionsSelectionPageControl.cs
* src/Ankh.UI/SvnLog/LogChangedPaths.cs
* src/Ankh.UI/SvnLog/LogControl.cs
* src/Ankh.UI/SvnLog/LogMessageView.cs
* src/Ankh.UI/SvnLog/LogRevisionControl.cs
* src/Ankh.UI/SvnLog/LogViewerDialog.cs
  Update callers and listeners

git-svn-id: https://ctf.open.collab.net/svn/repos/ankhsvn/trunk@5967 ecf7f2a6-7b29-9143-b1c6-df2d17ec8657

src/Ankh.UI/SvnLog/LogChangedPaths.cs
@@ -52,19 +52,19 @@ namespace Ankh.UI
             set { _dataSource = value; }
         }
 
-		IAnkhServiceProvider _context;
-		[Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-		public IAnkhServiceProvider Context
-		{
-			get { return _context; }
-			set
-			{
-				_context = value;
-				changedPaths.SelectionPublishServiceProvider = value;
+        IAnkhServiceProvider _context;
+        [Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public IAnkhServiceProvider Context
+        {
+            get { return _context; }
+            set
+            {
+                _context = value;
+                changedPaths.SelectionPublishServiceProvider = value;
 
 
-			}
-		}
+            }
+        }
 
         #region ICurrentItemDestination<ISvnLogItem> Members
         ICurrentItemSource<ISvnLogItem> itemSource;
@@ -75,14 +75,14 @@ namespace Ankh.UI
             {
                 if (itemSource != null)
                 {
-                    itemSource.SelectionChanged -= new SelectionChangedEventHandler<ISvnLogItem>(SelectionChanged);
-                    itemSource.FocusChanged -= new FocusChangedEventHandler<ISvnLogItem>(FocusChanged);
+                    itemSource.SelectionChanged -= new EventHandler<CurrentItemEventArgs<ISvnLogItem>>(SelectionChanged);
+                    itemSource.FocusChanged -= new EventHandler<CurrentItemEventArgs<ISvnLogItem>>(FocusChanged);
                 }
                 itemSource = value;
                 if (itemSource != null)
                 {
-                    itemSource.SelectionChanged += new SelectionChangedEventHandler<ISvnLogItem>(SelectionChanged);
-                    itemSource.FocusChanged += new FocusChangedEventHandler<ISvnLogItem>(FocusChanged);
+                    itemSource.SelectionChanged += new EventHandler<CurrentItemEventArgs<ISvnLogItem>>(SelectionChanged);
+                    itemSource.FocusChanged += new EventHandler<CurrentItemEventArgs<ISvnLogItem>>(FocusChanged);
                 }
 
             }
@@ -90,19 +90,19 @@ namespace Ankh.UI
 
         #endregion
 
-        
 
-        void SelectionChanged(object sender, IList<ISvnLogItem> e)
+
+        void SelectionChanged(object sender, CurrentItemEventArgs<ISvnLogItem> e)
         {
         }
 
-
-
-        void FocusChanged(object sender, ISvnLogItem e)
+        void FocusChanged(object sender, CurrentItemEventArgs<ISvnLogItem> e)
         {
             changedPaths.Items.Clear();
 
-            if (e != null && e.ChangedPaths != null)
+            ISvnLogItem item = e.Source.FocusedItem;
+
+            if (e != null && item.ChangedPaths != null)
             {
                 List<PathListViewItem> paths = new List<PathListViewItem>();
 
@@ -116,9 +116,9 @@ namespace Ankh.UI
                     origins.Add(origin.TrimEnd('/'));
                 }
 
-                foreach (SvnChangeItem i in e.ChangedPaths)
+                foreach (SvnChangeItem i in item.ChangedPaths)
                 {
-                    paths.Add(new PathListViewItem(changedPaths, e, i, e.RepositoryRoot, HasFocus(origins, i.Path)));
+                    paths.Add(new PathListViewItem(changedPaths, item, i, item.RepositoryRoot, HasFocus(origins, i.Path)));
                 }
 
                 changedPaths.Items.AddRange(paths.ToArray());
@@ -152,17 +152,17 @@ namespace Ankh.UI
             changedPaths.Items.Clear();
         }
 
-        
+
 
         private void changedPaths_ShowContextMenu(object sender, MouseEventArgs e)
         {
             Point p = MousePosition;
 
-			if(Context != null)
-			{
-				IAnkhCommandService cs = Context.GetService<IAnkhCommandService>();
-				cs.ShowContextMenu(AnkhCommandMenu.LogChangedPathsContextMenu, p.X, p.Y);
-			}
+            if (Context != null)
+            {
+                IAnkhCommandService cs = Context.GetService<IAnkhCommandService>();
+                cs.ShowContextMenu(AnkhCommandMenu.LogChangedPathsContextMenu, p.X, p.Y);
+            }
         }
 
         private void changedPaths_MouseDoubleClick(object sender, MouseEventArgs e)

src/Ankh.Services/UI/ICurrentItemSource.cs
@@ -31,12 +31,12 @@ namespace Ankh.UI
         /// <summary>
         /// Occurs when the selection has been changed
         /// </summary>
-        event SelectionChangedEventHandler<TItem> SelectionChanged;
+        event EventHandler<CurrentItemEventArgs<TItem>> SelectionChanged;
 
         /// <summary>
         /// Occurs when the focus has been changed
         /// </summary>
-        event FocusChangedEventHandler<TItem> FocusChanged;
+        event EventHandler<CurrentItemEventArgs<TItem>> FocusChanged;
 
         /// <summary>
         /// Gets the focused item
@@ -61,19 +61,19 @@ namespace Ankh.UI
         ICurrentItemSource<TItem> ItemSource { get;set;}
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <typeparam name="TItem"></typeparam>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    public delegate void SelectionChangedEventHandler<TItem>(object sender, IList<TItem> e);
+    public class CurrentItemEventArgs<TItem> : EventArgs
+    {
+        readonly ICurrentItemSource<TItem> _source;
+        public CurrentItemEventArgs(ICurrentItemSource<TItem> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            _source = source;
+        }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <typeparam name="TItem"></typeparam>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    public delegate void FocusChangedEventHandler<TItem>(object sender, TItem e);
+        public ICurrentItemSource<TItem> Source
+        {
+            get { return _source; }
+        }
+    }    
 }

src/Ankh.UI/MergeWizard/MergeRevisionsSelectionPageControl.cs
@@ -33,7 +33,7 @@ namespace Ankh.UI.MergeWizard
         public MergeRevisionsSelectionPageControl()
         {
             InitializeComponent();
-            logToolControl1.SelectionChanged += new SelectionChangedEventHandler<Ankh.Scc.ISvnLogItem>(logToolControl1_SelectionChanged);
+            logToolControl1.SelectionChanged += new EventHandler<CurrentItemEventArgs<ISvnLogItem>>(logToolControl1_SelectionChanged);
 
             logToolControl1.StrictNodeHistory = true;
         }
@@ -48,7 +48,7 @@ namespace Ankh.UI.MergeWizard
 
         public event EventHandler<EventArgs> SelectionChanged;
 
-        void logToolControl1_SelectionChanged(object sender, IList<Ankh.Scc.ISvnLogItem> e)
+        void logToolControl1_SelectionChanged(object sender, CurrentItemEventArgs<ISvnLogItem> e)
         {
             OnSelectionChanged(EventArgs.Empty);
         }

src/Ankh.UI/SvnLog/LogControl.cs
@@ -202,9 +202,9 @@ namespace Ankh.UI.SvnLog
 
         public event EventHandler<BatchFinishedEventArgs> BatchFinished;
 
-        public event SelectionChangedEventHandler<ISvnLogItem> SelectionChanged;
+        public event EventHandler<CurrentItemEventArgs<ISvnLogItem>> SelectionChanged;
 
-        public event FocusChangedEventHandler<ISvnLogItem> FocusChanged;
+        public event EventHandler<CurrentItemEventArgs<ISvnLogItem>> FocusChanged;
 
         public ISvnLogItem FocusedItem
         {
@@ -231,13 +231,13 @@ namespace Ankh.UI.SvnLog
             }
         }
 
-        void OnFocusChanged(object sender, ISvnLogItem e)
+        void OnFocusChanged(object sender, CurrentItemEventArgs<ISvnLogItem> e)
         {
             if(FocusChanged != null)
                 FocusChanged(sender, e);
         }
 
-        void OnSelectionChanged(object sender, IList<ISvnLogItem> e)
+        void OnSelectionChanged(object sender, CurrentItemEventArgs<ISvnLogItem> e)
         {
             if (SelectionChanged != null)
                 SelectionChanged(sender, e);

src/Ankh.UI/SvnLog/LogMessageView.cs
@@ -52,17 +52,16 @@ namespace Ankh.UI
             set 
             { 
                 if(logItemSource != null)
-                    logItemSource.FocusChanged -= new FocusChangedEventHandler<ISvnLogItem>(LogFocusChanged);
+                    logItemSource.FocusChanged -= new EventHandler<CurrentItemEventArgs<ISvnLogItem>>(LogFocusChanged);
 
                 logItemSource = value; 
                 
                 if(logItemSource != null)
-                    logItemSource.FocusChanged += new FocusChangedEventHandler<ISvnLogItem>(LogFocusChanged);
-
+                    logItemSource.FocusChanged += new EventHandler<CurrentItemEventArgs<ISvnLogItem>>(LogFocusChanged);
             }
         }
 
-        void LogFocusChanged(object sender, ISvnLogItem e)
+        void LogFocusChanged(object sender, CurrentItemEventArgs<ISvnLogItem> e)
         {
             if (ItemSource != null && ItemSource.FocusedItem != null)
                 logMessageEditor.Text = logItemSource.FocusedItem.LogMessage;

src/Ankh.UI/SvnLog/LogRevisionControl.cs
@@ -387,10 +387,9 @@ namespace Ankh.UI.SvnLog
         }
 
         #region ICurrentItemSource<ISvnLogItem> Members
+        public event EventHandler<CurrentItemEventArgs<ISvnLogItem>> SelectionChanged;
 
-        public event SelectionChangedEventHandler<ISvnLogItem> SelectionChanged;
-
-        public event FocusChangedEventHandler<ISvnLogItem> FocusChanged;
+        public event EventHandler<CurrentItemEventArgs<ISvnLogItem>> FocusChanged;
 
         public ISvnLogItem FocusedItem
         {
@@ -417,7 +416,7 @@ namespace Ankh.UI.SvnLog
         private void logRevisionControl1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
         {
             if (FocusChanged != null)
-                FocusChanged(this, FocusedItem);
+                FocusChanged(this, new CurrentItemEventArgs<ISvnLogItem>(this));
 
             ExtendList();
         }
@@ -429,7 +428,7 @@ namespace Ankh.UI.SvnLog
                 _selectedItems.Add(new LogItem((LogRevisionItem)logView.Items[i], LogSource.RepositoryRoot));
 
             if (SelectionChanged != null)
-                SelectionChanged(this, SelectedItems);
+                FocusChanged(this, new CurrentItemEventArgs<ISvnLogItem>(this));
         }
 
         private void logRevisionControl1_ShowContextMenu(object sender, MouseEventArgs e)
@@ -440,11 +439,6 @@ namespace Ankh.UI.SvnLog
             Point p = MousePosition;
             IAnkhCommandService cs = Context.GetService<IAnkhCommandService>();
             cs.ShowContextMenu(AnkhCommandMenu.LogViewerContextMenu, p.X, p.Y);
-        }
-
-        private void logRevisionControl1_CacheVirtualItems(object sender, CacheVirtualItemsEventArgs e)
-        {
-            //Debug.WriteLine(string.Format("CacheVirtualItems start: {0}, end: {1}", e.StartIndex, e.EndIndex));
         }
     }
 

src/Ankh.UI/SvnLog/LogViewerDialog.cs
@@ -36,7 +36,6 @@ namespace Ankh.UI
         public LogViewerDialog()
         {
             InitializeComponent();
-            logViewerControl.SelectionChanged += new SelectionChangedEventHandler<ISvnLogItem>(value_SelectionChanged); ;
         }
 
         public LogViewerDialog(SvnOrigin target, IAnkhServiceProvider context)
@@ -69,21 +68,17 @@ namespace Ankh.UI
             get { return logViewerControl.SelectedItems; }
         }
 
-        #region UI Events
-        private void LogViewerDialog_Load(object sender, EventArgs e)
+        protected override void OnLoad(EventArgs e)
         {
-            logViewerControl.Site = Site;
+            base.OnLoad(e);
 
+            if (DesignMode)
+                return;
+        
             if (LogTarget == null)
                 throw new InvalidOperationException("Log target is null");
 
             logViewerControl.StartLog(Context, new SvnOrigin[] { LogTarget }, null, null);
-        }
-        #endregion
-
-
-        void value_SelectionChanged(object sender, IList<ISvnLogItem> e)
-        {
         }
     }
 }

commit 1c9c9b65be0391a4ed2e3a653029a1c1c7103592

* src/Ankh.UI/SvnLog/LogChangedPaths.cs   Resolve null reference exception introduced by refactoring in r5967.

src/Ankh.UI/SvnLog/LogChangedPaths.cs
@@ -102,7 +102,7 @@ namespace Ankh.UI
 
             ISvnLogItem item = e.Source.FocusedItem;
 
-            if (e != null && item.ChangedPaths != null)
+            if (item != null && item.ChangedPaths != null)
             {
                 List<PathListViewItem> paths = new List<PathListViewItem>();
 